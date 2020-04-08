ab7bd9d7aba602715557e318479c934f5f458155
Katelyn Booth <katelynbooth@katelyns-mbp.lan>
2020-04-08 13:16:13 -0400

    first commnit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SocialPostsComponent } from './social-posts/social-posts.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SocialPostsComponent,
+    PostComponent,
+    PostFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  post: Post[] = [
+    {
+      title: "Grand Circus",
+      thought: "Grand Circus is cool."
+    },
+    {
+      title: "Ritual",
+      thought: "Ritual is a well developed app."
+    },
+    {
+      title: "Scott",
+      thought: "Scott rides scooters."
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
